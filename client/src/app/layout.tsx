d249ffa4f9b0eef9809e4ed2766cdd0d79cd948d
Samuel Jauregui <samjaurz_@hotmail.com>
2025-10-03 02:54:36 -0700

    Changing to tailwind 3 and modified de manual UI

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import NavBar from '@/components/Navbar';
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,11 +25,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <NavBar />
         {children}
       </body>
     </html>
   );
 }
+
